67bdd5bba0cb4a9eee41bafe352e995bd73aa407
saidul-bhuiyan135 <si2758830@gmail.com>
2025-03-07 19:52:27 +0600

    Update top.js

--- scripts/cmds/top.js
+++ scripts/cmds/top.js
@@ -19,7 +19,7 @@ module.exports = {
     const b = await usersData.getAll(),
       c = b.sort((b, c) => c.money - b.money).slice(0, 15),
       d = c.map((b, c) => `${c + 1}. ${b.name}: ☞ ${a(b.money)} 💲`),
-      e = `🎉 𝚃𝙾𝙿 15 𝚁𝙸𝙲𝙷𝙴𝚂𝚃 𝚄𝚂𝙴𝚁𝚂 🎉\n \n${d.join('\n \n')}\n\n🌟𝙺𝚎𝚎𝚙 𝚎𝚊𝚛𝚗𝚒𝚗𝚐 𝚝𝚘 𝚌𝚕𝚒𝚖𝚋 𝚝𝚑𝚎 𝚛𝚊𝚗𝚔𝚜🌟`;
+      e = ` 𝚃𝙾𝙿 15 𝚁𝙸𝙲𝙷𝙴𝚂𝚃 𝚄𝚂𝙴𝚁𝚂 💰\n \n${d.join('\n \n')}\n\n🌟𝙺𝚎𝚎𝚙 𝚎𝚊𝚛𝚗𝚒𝚗𝚐 𝚝𝚘 𝚌𝚕𝚒𝚖𝚋 𝚝𝚑𝚎 𝚛𝚊𝚗𝚔𝚜🌟`;
     
     message.reply(e);
   }
